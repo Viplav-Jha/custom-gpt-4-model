8cc4751280570027e9b4c9b6500a6b155205b6b6
Viplav jha <viplavjha1993@gmail.com>
2024-01-05 16:55:20 +0530

    added requied file

---
+++ src/app/[[...chatId]]/chat-content.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function chatContent() {
+  return (
+    <>
+    <div className="max-w-4xl w-full mx-auto flex-1 px-10 py-5 overflow-x-hidden overflow-y-auto">
+       AI Comtent goes here
+    </div>
+    <ChatInput onSubmit={handleSubmit}
+    </>
+  )
+}
