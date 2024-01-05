43c9149455ea8165f4342ffe37bab37766f3c5d7
Viplav jha <viplavjha1993@gmail.com>
2024-01-05 19:17:54 +0530

    initial commit added

--- src/app/[[...chatId]]/chat-content.tsx
+++ src/app/[[...chatId]]/chat-content.tsx
@@ -5,12 +5,17 @@ import ChatInput from "@/components/chat-input"
 
 
 export default function chatContent() {
+  
+    const handleSubmit =()=>{
+        console.log("submit")
+    }
+
   return (
     <>
     <div className="max-w-4xl w-full mx-auto flex-1 px-10 py-5 overflow-x-hidden overflow-y-auto">
        AI Comtent goes here
     </div>
-    <ChatInput/>
+    <ChatInput onSubmit={handleSubmit}/>
     </>
   )
 }
