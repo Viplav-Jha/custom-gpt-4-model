651c29051d4a9ac919643c401f1a03590fb88a6e
Viplav jha <viplavjha1993@gmail.com>
2024-01-05 17:02:50 +0530

    added required file

--- src/app/[[...chatId]]/chat-content.tsx
+++ src/app/[[...chatId]]/chat-content.tsx
@@ -1,12 +1,16 @@
 "use client"
 
+import ChatInput from "@/components/chat-input"
+
+
+
 export default function chatContent() {
   return (
     <>
     <div className="max-w-4xl w-full mx-auto flex-1 px-10 py-5 overflow-x-hidden overflow-y-auto">
        AI Comtent goes here
     </div>
-    <ChatInput onSubmit={handleSubmit}
+    <ChatInput/>
     </>
   )
 }
