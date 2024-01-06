d3f3df0f64ac61447ca4b794e1635403144e225d
Viplav jha <viplavjha1993@gmail.com>
2024-01-06 15:00:49 +0530

    rchat input update

--- src/app/[[...chatId]]/chat-content.tsx
+++ src/app/[[...chatId]]/chat-content.tsx
@@ -1,21 +1,47 @@
-"use client"
+import React from 'react';
+import ChatInput from '@/components/chat-input';
 
-import ChatInput from "@/components/chat-input"
+export default function ChatContent() {
+  const handleSubmit = async (value: string, file?: File) => {
+    try {
+      const res = await fetch("/api/message", {
+        method: "POST",
+        body: JSON.stringify({ content: value }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
+      if (!res.ok || !res.body) {
+        alert("Error sending message");
+        return;
+      }
 
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
+      let finalResult = "";
 
-export default function chatContent() {
-  
-    const handleSubmit =()=>{
-        console.log("submit")
+      while (true) {
+        const { value, done } = await reader.read();
+        const text = decoder.decode(value);
+        finalResult += text;
+        console.log(finalResult);
+
+        if (done) {
+          break
+        }
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
     }
+  };
 
   return (
     <>
-    <div className="max-w-4xl w-full mx-auto flex-1 px-10 py-5 overflow-x-hidden overflow-y-auto">
-       AI Comtent goes here
-    </div>
-    <ChatInput onSubmit={handleSubmit}/>
+      <div className="max-w-4xl w-full mx-auto flex-1 px-10 py-5 overflow-x-hidden overflow-y-auto">
+        {/* Render AI content here */}
+      </div>
+      <ChatInput onSubmit={handleSubmit} />
     </>
-  )
+  );
 }
