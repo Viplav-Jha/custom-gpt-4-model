d57ca853cb70e9ff742609e9b5b24ec6e65d6985
Viplav jha <viplavjha1993@gmail.com>
2024-01-06 15:01:11 +0530

    message route

--- src/app/message/route.ts
+++ src/app/message/route.ts
@@ -1,3 +1,23 @@
-export async function POST(req:Request){
-    return new Response('ok')
-};
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: Request) {
+  const { content } = await req.json();
+
+  // send  that openai
+
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content }],
+    model: "gpt-4-vision-preview",
+    stream:true
+  });
+  console.log({content});
+
+  const stream = OpenAIStream(chatCompletion)
+ 
+  return new StreamingTextResponse(stream)
+}
